ba27258ff6c93249cb5ee131ff42e783e260014e
saffr3n <saffr3n@gmail.com>
2025-07-12 17:01:08 +0300

    Complete exercise 13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  const importantChars = string
+    .toLowerCase()
+    .split("")
+    .filter((c) => /[a-z0-9]/.test(c));
+  const reversed = importantChars.toReversed();
+  return importantChars.join("") === reversed.join("");
 };
 
 // Do not edit below this line
