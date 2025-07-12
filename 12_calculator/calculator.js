4182e4ee2dbce40d64f9f845452e4308b6828628
saffr3n <saffr3n@gmail.com>
2025-07-12 16:50:21 +0300

    Complete exercise 12

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (nums) {
+  return nums.reduce((res, n) => res + n, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (nums) {
+  return nums.reduce((res, n) => res * n, 1);
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function (n) {
+  if (n === 0) return 1;
+  let res = n;
+  for (let i = n - 1; i > 0; i--) {
+    res *= i;
+  }
+  return res;
 };
 
 // Do not edit below this line
@@ -29,5 +34,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
