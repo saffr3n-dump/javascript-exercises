d85de0705f6f6cf79b5f0f42d191f7847bc3b396
saffr3n <saffr3n@gmail.com>
2025-07-12 17:25:53 +0300

    Complete exercise 14

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function (n) {
+  n = Number(n);
+  if (n < 0) return "OOPS";
+  let temp;
+  let curr = 0;
+  let next = 1;
+  for (let i = 0; i < n; i++) {
+    temp = curr + next;
+    curr = next;
+    next = temp;
+  }
+  return curr;
 };
 
 // Do not edit below this line
