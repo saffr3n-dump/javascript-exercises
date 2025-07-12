49ad11359200aa759d4c6412f087d14422b62e32
saffr3n <saffr3n@gmail.com>
2025-07-12 17:37:22 +0300

    Complete exercise 16

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (arr) {
+  return arr.sort((a, b) => {
+    const currentYear = new Date().getFullYear();
+    const ageA = (a.yearOfDeath ?? currentYear) - a.yearOfBirth;
+    const ageB = (b.yearOfDeath ?? currentYear) - b.yearOfBirth;
+    return ageB - ageA;
+  })[0];
 };
 
 // Do not edit below this line
